102c45bc642a960936f8d21418f09d10e6df6f63
Kyle Hiroshi Young <93508000+HirotheChamp@users.noreply.github.com>
2022-09-24 15:01:40 -0700

    continued to build sidebar

--- client/src/components/Sidebar/Sidebar.jsx
+++ client/src/components/Sidebar/Sidebar.jsx
@@ -1,7 +1,7 @@
 import React from 'react'
 import './Sidebar.css'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-import { faCopyright, faThLarge } from '@fortawesome/free-solid-svg-icons'
+import { faAngleDown, faCopyright, faThLarge } from '@fortawesome/free-solid-svg-icons'
 
 
 const Sidebar = () => {
@@ -9,7 +9,7 @@ const Sidebar = () => {
  <div className="sidebar">
   <div className="logo-details">
   
-  <FontAwesomeIcon icon={faCopyright} className="nav-icon"/>
+  <FontAwesomeIcon icon={faCopyright} className="copyright-icon"/>
     <span className="logo_name">Hiro's Landing</span>
   </div>
   <ul className="nav-links">
@@ -28,6 +28,7 @@ const Sidebar = () => {
       
     <span className="link_name">Category</span>
       </a>
+      <FontAwesomeIcon icon={faAngleDown} className="nav-icons"/>
     </div>
     </li>
   </ul>

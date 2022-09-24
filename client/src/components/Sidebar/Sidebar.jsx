819f79f1e06378d1d26663a89f9f0c86ac289974
Kyle Hiroshi Young <93508000+HirotheChamp@users.noreply.github.com>
2022-09-23 17:59:02 -0700

    started on sidebar, ran into issue with formatting the navbar icons.

--- client/src/components/Sidebar/Sidebar.jsx
+++ client/src/components/Sidebar/Sidebar.jsx
@@ -1,16 +1,37 @@
 import React from 'react'
 import './Sidebar.css'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faCopyright, faThLarge } from '@fortawesome/free-solid-svg-icons'
+
 
 const Sidebar = () => {
   return (
-    <div className='sidebar'>
-<ul className='sidebar-menu-items'>
-  <li>Home</li>
-  <li>Profile</li>
-  <li>Menu</li>
-</ul>
-
+ <div className="sidebar">
+  <div className="logo-details">
+  
+  <FontAwesomeIcon icon={faCopyright} className="nav-icon"/>
+    <span className="logo_name">Hiro's Landing</span>
+  </div>
+  <ul className="nav-links">
+    <li>
+      <a href='#'>
+      <FontAwesomeIcon icon={faThLarge} className="nav-icons"/>
+      
+    <span className="link_name">Dashboard</span>
+      </a>
+    </li>
+   
+    <li>
+    <div className="icon-links">
+    <a href='#'>
+      <FontAwesomeIcon icon={faThLarge} className="nav-icons"/>
+      
+    <span className="link_name">Category</span>
+      </a>
     </div>
+    </li>
+  </ul>
+ </div>
   )
 }
 

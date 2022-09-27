b0c460f74871026d4a72117672f691dbdfb2c7a5
Kyle Hiroshi Young <93508000+HirotheChamp@users.noreply.github.com>
2022-09-27 11:40:33 -0700

    started adding categories and subcategories to the sidebar

--- client/src/components/Sidebar/Sidebar.jsx
+++ client/src/components/Sidebar/Sidebar.jsx
@@ -6,7 +6,7 @@ import { faAngleDown, faCopyright, faThLarge } from '@fortawesome/free-solid-svg
 
 const Sidebar = () => {
   return (
- <div className="sidebar">
+ <div className="sidebar close">
   <div className="logo-details">
   
   <FontAwesomeIcon icon={faCopyright} className="copyright-icon"/>
@@ -19,6 +19,10 @@ const Sidebar = () => {
       
     <span className="link_name">Dashboard</span>
       </a>
+      <ul className="sub-menu blank">
+      <li><a className="link_name" href='#'>Post</a></li>
+     
+    </ul>
     </li>
    
     <li>
@@ -31,9 +35,10 @@ const Sidebar = () => {
       <FontAwesomeIcon icon={faAngleDown} className="nav-icons"/>
     </div>
     <ul className="sub-menu">
-      <li><a href='#'>Web Design</a></li>
-      <li><a href='#'>Web Design</a></li>
-      <li><a href='#'>Web Design</a></li>
+      <li><a className="link_name" href='#'>Post</a></li>
+      <li><a href='#'>HTML</a></li>
+      <li><a href='#'>JAVA</a></li>
+      <li><a href='#'>JavaScript</a></li>
     </ul>
     </li>
 
@@ -46,6 +51,24 @@ const Sidebar = () => {
       </a>
       <FontAwesomeIcon icon={faAngleDown} className="nav-icons"/>
     </div>
+    <ul className="sub-menu">
+      <li><a className="link_name" href='#'>Posts</a></li>
+      <li><a href='#'>Web Design</a></li>
+      <li><a href='#'>Login Form</a></li>
+      <li><a href='#'>Card Design</a></li>
+    
+    </ul>
+    </li>
+    <li>
+      <a href='#'>
+      <FontAwesomeIcon icon={faThLarge} className="nav-icons"/>
+      
+    <span className="link_name">Analytics</span>
+      </a>
+      <ul className="sub-menu blank">
+      <li><a className="link_name" href='#'>Analytics</a></li>
+     
+    </ul>
     </li>
   </ul>
  </div>

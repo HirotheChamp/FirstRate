5115d8484568f5151562756b1dfb1874bd469247
Kyle Hiroshi Young <93508000+HirotheChamp@users.noreply.github.com>
2022-09-29 17:08:46 -0700

    started adding a chat messenger feature

--- client/src/App.js
+++ client/src/App.js
@@ -5,11 +5,23 @@ import Login from './components/LoginRegistration/Login'
 import Register from './components/LoginRegistration/Register'
 import Dashboard from './pages/Dashboard/Dashboard'
 import LandingPage from './pages/LandingPage/LandingPage'
+import {StreamChat} from 'stream-chat'
+import {Chat} from 'stream-chat-react'
+import Cookies from 'universal-cookie'
+
+const apiKey = 'j4mg6np3pw59';
+const client = StreamChat.getInstance(apiKey)
 
 function App() {
   return (
+    <div className='app-wrapper'>
    <BrowserRouter>
    <Switch>
+
+<Chat client={client} theme="team light">
+  <ChannelListContainer/>
+  <ChannelContainer/>
+</Chat>
 
     <Route exact path="/"  component={LandingPage}/> 
     <Route exact path="/login"  component={Login}/>
@@ -23,6 +35,7 @@ function App() {
    
    
    </BrowserRouter>
+   </div>
   );
 }
 
